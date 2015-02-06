b10cbdc35d3e32f9f3193d39752528d391e98ea5
Isaac Pateau <isaacpateau05@gmail.com>
2015-02-06 16:36:49 -0500

    Shbootimg changes:
    - Directly call ramdisk-recovery.cpio so it can get generated
    - Remove the need from the source busybox as it gets too large see this commit
      for more details:
      https://github.com/zaclimon/android_kernel_samsung_aries/commit/f3c2205f27a88def5ba10b8025024d7524068864

--- shbootimg.mk
+++ shbootimg.mk
@@ -6,7 +6,7 @@ $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	$(MINIGZIP) -d < $(INSTALLED_RAMDISK_TARGET) > $@
 
 # Add ramdisk dependencies to kernel
-TARGET_KERNEL_BINARIES: $(recovery_ramdisk) $(uncompressed_ramdisk) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image $(PRODUCT_OUT)/utilities/bml_over_mtd
+TARGET_KERNEL_BINARIES: $(PRODUCT_OUT)/ramdisk-recovery.cpio $(uncompressed_ramdisk) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/symbols/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image $(PRODUCT_OUT)/utilities/bml_over_mtd
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET)
